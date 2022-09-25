1d7176f8f469c575d1f0a61543e63f3c9311616e
Weitingbai <weitingbai408@gmail.com>
2022-09-25 22:29:46 +0800

    Initial Commit

---
+++ DEMO1/DEMO1App.swift
@@ -0,0 +1,17 @@
+//
+//  DEMO1App.swift
+//  DEMO1
+//
+//  Created by Caren Chin on 2022/9/23.
+//
+
+import SwiftUI
+
+@main
+struct DEMO1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
